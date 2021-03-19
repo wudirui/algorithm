41a062cc70c7ece67eba5de9814328750871a899
zhangrui <1570266831@qq.com>
2021-03-19 10:02:49 +0800

    leetCode 第二题 两数相加之链表 之修改

--- src/test2/TwoIntegerLinkListSum.java
+++ src/test2/TwoIntegerLinkListSum.java
@@ -15,7 +15,19 @@ class Node {
 
 public class TwoIntegerLinkListSum {
     public static void main(String[] args) {
+        Node node1 = new Node(9);
+        node1.next = new Node(9);
+        node1.next.next = new Node(9);
 
+        Node node2 = new Node(9);
+        node2.next = new Node(9);
+        node2.next.next = new Node(9);
+        node2.next.next.next = new Node(9);
+        Node sum = getSum(node1, node2);
+        while (sum != null) {
+            System.out.println(sum.value);
+            sum = sum.next;
+        }
     }
 
     /**
@@ -29,25 +41,25 @@ public class TwoIntegerLinkListSum {
         Node head = new Node(0);
         Node curr = head;
         while (node1 != null && node2 != null) {
-            curr.value = (node1.value + node2.value + curr.value) % 10;
             curr.next = new Node((node1.value + node2.value + curr.value) / 10);
+            curr.value = (node1.value + node2.value + curr.value) % 10;
             curr = curr.next;
             node1 = node1.next;
             node2 = node2.next;
         }
         while (node1 != null) {
+            curr.next = new Node((node1.value + curr.value) / 10);
             curr.value = (node1.value + curr.value) % 10;
-            curr.next = new Node ((node1.value + curr.value) / 10);
             curr = curr.next;
             node1 = node1.next;
         }
         while (node2 != null) {
-            curr.value = (node2.value + curr.value) % 10;
             curr.next = new Node((node2.value + curr.value) / 10);
+            curr.value = (node2.value + curr.value) % 10;
             curr = curr.next;
             node2 = node2.next;
         }
-        if (curr.value ==0){
+        if (curr.value == 0) {
             curr = null;
         }
         return head;
